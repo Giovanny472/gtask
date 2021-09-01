bd3b36a648b87f933ceeebc8908c83db78fae022
Giovanny472 <43882462+Giovanny472@users.noreply.github.com>
2021-09-01 12:21:43 +0300

    создание сущности user

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,1 +1,18 @@
+package user
+
+// cущность JSON для запроса клиента
+type User struct {
+	Login     string `json:"login"`
+	Passworld string `json:""`
+	LastName  string `json:"lastname"`
+}
+
+// cущность для получения данных из БД
+type UserDB struct {
+	Status   int    `json:"status"`
+	Id       int    `json:"id"`
+	Login    string `json:"login"`
+	Name     string `json:"name"`
+	LastName string `json:"lastname"`
+}
 
