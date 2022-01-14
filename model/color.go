9394eb285af6d72d001fd8d8565e569d2cb0f0ff
giovanny <giovanny@hq.bereg.net>
2022-01-14 11:31:38 +0300

    очистить терминал

--- model/color.go
+++ model/color.go
@@ -1,16 +1,40 @@
 package model
 
+import (
+	"runtime"
+)
+
 type Color string
 
-const (
-	Black     Color = "\033[0;30m"
-	Red       Color = "\033[0;31m"
-	RedBold   Color = "\033[1;31m"
-	Green     Color = "\033[0;32m"
-	GreenBold Color = "\033[1;32m"
-	Yellow    Color = "\033[0;33m"
-	Blue      Color = "\033[0;34m"
-	Purple    Color = "\033[0;35m"
-	Cyan      Color = "\033[0;36m"
-	White     Color = "\033[0;37m"
+var (
+	ColorReset Color = "\033[0m"
+	Black      Color = "\033[0;30m"
+	Red        Color = "\033[0;31m"
+	RedBold    Color = "\033[1;31m"
+	Green      Color = "\033[0;32m"
+	GreenBold  Color = "\033[1;32m"
+	Yellow     Color = "\033[0;33m"
+	Blue       Color = "\033[0;34m"
+	Purple     Color = "\033[0;35m"
+	Cyan       Color = "\033[36m"
+	CyanWin    Color = "\033[0;36m"
+	White      Color = "\033[0;37m"
 )
+
+func init() {
+
+	if runtime.GOOS == "windows" {
+		ColorReset = ""
+		Black = ""
+		Red = ""
+		RedBold = ""
+		Green = ""
+		GreenBold = ""
+		Yellow = ""
+		Blue = ""
+		Purple = ""
+		Cyan = ""
+		CyanWin = ""
+		White = ""
+	}
+}
--- pkg/ui/terminal.go
+++ pkg/ui/terminal.go
@@ -2,6 +2,9 @@ package ui
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/Giovanny472/gtask/model"
 )
@@ -15,27 +18,26 @@ func NewUITerminal() model.Shower {
 
 func (uiterm *uiterminal) Show(listask *model.ListTask) {
 
-	//fmt.Println(model.Cyan, "********************************")
-	//fmt.Println(model.Cyan, "*      З А Д А Ч И             *")
-	//fmt.Println(model.Cyan, "********************************")
-	fmt.Println("********************************")
-	fmt.Println("*      З А Д А Ч И             *")
-	fmt.Println("********************************")
+	uiterm.clearTerminal()
+
+	fmt.Println(model.Cyan, "********************************")
+	fmt.Println(model.Cyan, "*      З А Д А Ч И             *")
+	fmt.Println(model.Cyan, "********************************")
 
 	//   список задачи
-	//var acolor model.Color
+	var acolor model.Color
 	for idx, value := range *listask {
 
-		//if value.Progress > 90 {
-		//	acolor = model.GreenBold
-		//} else if value.Progress < 50 {
-		//	acolor = model.RedBold
-		//} else {
-		//	acolor = model.Cyan
-		//}
+		if value.Progress > 90 {
+			acolor = model.GreenBold
+		} else if value.Progress < 50 {
+			acolor = model.RedBold
+		} else {
+			acolor = model.Cyan
+		}
 
-		//fmt.Print(model.Cyan, "[", model.Cyan, idx+1, "]")
-		fmt.Print("[", idx+1, "]")
+		// счетчик
+		fmt.Print(model.Cyan, "[", model.Cyan, idx+1, "]")
 
 		if idx < 10 {
 			fmt.Print("  ")
@@ -44,11 +46,24 @@ func (uiterm *uiterminal) Show(listask *model.ListTask) {
 		}
 
 		// название задачи
-		//fmt.Print(model.Cyan, value.Name)
-		fmt.Print(value.Name)
-		fmt.Print(" ..... ")
+		fmt.Print(model.Cyan, value.Name)
+		fmt.Print(model.Cyan, " ..... ")
 
-		//fmt.Println(acolor, value.Progress, "%")
-		fmt.Println(value.Progress, "%")
+		fmt.Println(acolor, value.Progress, "%")
+		fmt.Print(model.ColorReset)
 	}
+}
+
+func (uiterm *uiterminal) clearTerminal() {
+
+	var cmd *exec.Cmd
+
+	if runtime.GOOS == "linux" {
+		cmd = exec.Command("clear")
+	} else if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
